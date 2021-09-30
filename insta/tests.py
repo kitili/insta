0c4d2320a1ecf98240c48975ade699a8d652d58c
Your Name <you@example.com>
2021-09-30 11:59:10 +0300

    insta

---
+++ insta/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from . models import Comment
+
+
+
+class CommentTestClass(TestCase):
+
+    def setUp(self):
+        
+        self.new_comment = Comment(comment= "comment")
+        self.new_comment.save()
