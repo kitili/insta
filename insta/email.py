26dd224fc4be451bf93f26bb897cd71ed81343aa
Your Name <you@example.com>
2021-09-30 15:49:31 +0300

    insta

--- insta/email.py
+++ insta/email.py
@@ -4,7 +4,7 @@ from django.template.loader import render_to_string
 def send_welcome_email(name,receiver):
     # Creating message subject and sender
     subject = 'Welcome to the Instagram '
-    sender = 'nyambucindy0@gmail.com'
+    sender = 'mourinekitilimourine@gmail.com'
 
     #passing in the context vairables
     text_content = render_to_string('email/newsemail.txt',{"name": name})
